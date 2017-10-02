a3527c5f9e4d249059507698c12310bda4e1e58e
cyeef <cyeef@icloud.com>
2017-10-02 09:39:29 -0600

    Simple read file example.

--- src/edu/cnm/deepdive/io/Fille.java
+++ src/edu/cnm/deepdive/io/Fille.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 // import java.sql.Struct;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class Fille {
 
   private static final String FILENAME = "largest-product-data.txt";
 
-  public static String[] getline(String filename)
+  public static String[] getLines(String filename)
       throws FileNotFoundException, IOException {
 
     String[] result = null;
@@ -34,7 +35,7 @@ public class Fille {
           lines.add(line);
         }
       }
-        return lines.toArray(new String[0]);
+      return lines.toArray(new String[0]);
         /*
          for (String line = buffer.readLine();
          line ! = null;
@@ -61,17 +62,29 @@ public class Fille {
       }
     }*/
 
-      }
+    }
+  }
+      public static int[][] getMatrix(String[] lines, String delimiter) {
+       int[][] data = new int[lines.length][];
+       for (int i = 0; i <lines.length; i++) {
+         String[] parts =lines[i].split(delimiter);
+         int[] row = new int[parts.length];
+         for (int j = 0; j <parts.length; j++) {
+          row[j] = Integer.parseInt(parts[j]);
+         }
+         data[i] = row;
+       }
+       return data;
     }
 
-  public static void main(String[] args)  {
-    try {
-      for (String line : getline(FILENAME)) {
-        System.out.println(line);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+
+  public static void main(String[] args)
+    throws IOException {
+    for (int[]  row: getMatrix(getLines(FILENAME),  "\\s")) {
+        System.out.println(Arrays.toString(row));
+
     }
+
 
   }
 
